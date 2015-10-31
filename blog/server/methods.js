9f5b055c189d23778f6943a39ff4744afd8fa3f9
Nicolás López <nicolaslopezj@me.com>
2015-10-31 13:35:16 -0300

    change to 3 flags

--- blog/server/methods.js
+++ blog/server/methods.js
@@ -1,11 +1,11 @@
 Meteor.methods({
   flagPost: function(postId) {
     check(postId, String);
-    var ip = this.connection.clientAddress;
+    var ip = this.connection && this.connection.clientAddress;
     var post = Posts.findOne(postId);
-    if (!post) {
+    if (!post || !ip) {
       return;
-    } else if (post.flaggedBy && post.flaggedBy.length > 3) {
+    } else if (post.flaggedBy && post.flaggedBy.length > 1) {
       Posts.remove(postId);
     } else {
       Posts.update(postId, { $addToSet: { flaggedBy: ip } });
